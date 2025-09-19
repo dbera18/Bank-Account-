5f4e0ce47c5a31ad3e1cb82b97ddf926cb09f562
Dev_Dipankar_Bera <dbera@charlotte.edu>
2025-09-18 21:48:01 -0400

    changes for debugging

--- savings_account.py
+++ savings_account.py
@@ -1,8 +1,8 @@
 from bank_account import BankAccount
 
 class SavingsAccount(BankAccount):
-    def _init_(self, customer_name, current_balance, minimum_balance, account_number, routing_number, interest_rate=0.02):
-        super()._init_(customer_name, current_balance, minimum_balance, account_number, routing_number)
+    def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number, interest_rate=0.02):
+        super().__init__(customer_name, current_balance, minimum_balance, account_number, routing_number)
         self._interest_rate = interest_rate
 
     def apply_interest(self):
