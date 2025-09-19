15d90e34d8cc8b6aa1da4a10651f8f8b4a59c070
Dev_Dipankar_Bera <dbera@charlotte.edu>
2025-09-18 21:01:14 -0400

    Added Transfer method in Checking

--- checking_account.py
+++ checking_account.py
@@ -4,3 +4,13 @@ class CheckingAccount(BankAccount):
     def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number, transfer_limit=500):
         super().__init__(customer_name, current_balance, minimum_balance, account_number, routing_number)
         self._transfer_limit = transfer_limit
+
+    def transfer(self, amount, other_account):
+        if amount > self._transfer_limit:
+            return f"Transfer failed: exceeds transfer limit of {self._transfer_limit}"
+        elif self._current_balance - amount < self._minimum_balance:
+            return "Transfer failed: insufficient funds."
+        else:
+            self._current_balance -= amount
+            other_account.deposit(amount)
+            return f"Transferred {amount} to {other_account.customer_name}"
