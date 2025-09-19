c5a3474c27d6557046305bca8c8d6ba429e8e6a7
Dev_Dipankar_Bera <dbera@charlotte.edu>
2025-09-18 20:57:57 -0400

    Added constructor in CheckingAccount

--- checking_account.py
+++ checking_account.py
@@ -1,1 +1,6 @@
 from bank_account import BankAccount
+
+class CheckingAccount(BankAccount):
+    def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number, transfer_limit=500):
+        super().__init__(customer_name, current_balance, minimum_balance, account_number, routing_number)
+        self._transfer_limit = transfer_limit
